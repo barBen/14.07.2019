c18f68e220d635493c718dd5da066353705c58ba
barBen <bar180999@gmail.com>
2019-07-14 14:06:48 +0300

    Add files via upload

---
+++ FactoryMethodsln/saguie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Saguie : Vehicle
+    {
+
+        protected string name;
+
+        public Saguie(string model, float cylinder, string color, int numberOfWheels, string name) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return "Saguie : " + base.ToString() + $" { name}";
+        }
+    }
+}
---
+++ FactoryMethodsln/track.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Track : Vehicle
+    {
+
+        protected string WheelsSize;
+
+        public Track(string model, float cylinder, string color, int numberOfWheels, string WheelsSize) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.WheelsSize = WheelsSize;
+        }
+
+        public override string ToString()
+        {
+            return "Track : " + base.ToString() + $" { WheelsSize}";
+        }
+    }
+}
