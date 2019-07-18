82a9c75b05fc7395ad8d6536f5f1c95dc385c349
barBen <bar180999@gmail.com>
2019-07-18 13:10:00 +0300

    Add files via upload

---
+++ Builder/HumburgerFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class HumburgerFactory
+    {
+        public Hamburger createHamburger(string newHamburger)
+        {
+            switch (newHamburger)
+            {
+                case ("veg"):
+                    {
+                        VeggHamburgerBuilder vegBurger = new VeggHamburgerBuilder();
+                        vegBurger.BuildHamburger();
+                        return vegBurger.GetHamburger();
+                    }
+                case ("meat"):
+                    {
+                        MeatburgerBuilder meatBurger = new MeatburgerBuilder();
+                        meatBurger.BuildHamburger();
+                        return meatBurger.GetHamburger();
+                    }
+                case ("Lettuce"):
+                    {
+                        VeggHambBuilderLettuce LettuceBurger = new VeggHambBuilderLettuce();
+                        LettuceBurger.BuildHamburger();
+                        return LettuceBurger.GetHamburger();
+                    }
+                default:
+                    throw new HamburgerNotReadyException("Hamburger doent exsit."); ; 
+            }
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HamburgerBuilder> menu = new List<HamburgerBuilder>();
+            menu.Add(new VeggHamburgerBuilder());
+            menu.Add(new MeatburgerBuilder());
+            menu.Add(new VeggHambBuilderLettuce());
+
+            Hamburger hamb = PrepareHamburger(menu[2]);
+
+            Console.WriteLine(hamb);
+
+            //////////////////////////////////////////////////////////////
+
+            HumburgerFactory factory = new HumburgerFactory();
+
+            List<Hamburger> hums = new List<Hamburger>();
+            hums.Add(factory.createHamburger("veg"));
+            hums.Add(factory.createHamburger("meat"));
+
+            foreach (var hum in hums)
+            {
+                Console.WriteLine(hum);
+            }
+        }
+
+        static Hamburger PrepareHamburger(HamburgerBuilder builder)
+        {
+            builder.BuildHamburger();
+            return builder.GetHamburger();
+        }
+    }
+}
